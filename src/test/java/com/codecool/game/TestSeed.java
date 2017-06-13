bcc3731f993f7da365fe4baee01c6020523a5980
Diana Tokarska <tokarskadiana@gmail.com>
2017-06-13 11:38:53 +0200

    Wright tests to Seed enum

---
+++ src/test/java/com/codecool/game/TestSeed.java
@@ -0,0 +1,31 @@
+package com.codecool.game;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.codecool.game.Seed;
+
+public class TestSeed {
+
+    @Test
+    public void checkIfClassTypeIsEnum() {
+        assertTrue(Seed.class.isEnum());
+    }
+
+    @Test
+    public void CheckIfEmptyExists(){
+        assertNotNull(Seed.valueOf("EMPTY"));
+    }
+
+    @Test
+    public void CheckIfCrossExists(){
+        assertNotNull(Seed.valueOf("CROSS"));
+    }
+
+    @Test
+    public void CheckIfNoughtExists(){
+        assertNotNull(Seed.valueOf("NOUGHT"));
+    }
+
+
+}
