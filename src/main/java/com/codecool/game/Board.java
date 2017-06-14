43b22f64d2436e90306bfc7cc3fd1d6bb5f48d42
Marta Sajdak <crappylime@gmail.com>
2017-06-14 15:03:48 +0200

    Improve init board implementation

--- src/main/java/com/codecool/game/Board.java
+++ src/main/java/com/codecool/game/Board.java
@@ -5,21 +5,13 @@ public class Board {
     private Cell[][] cells;
 
     public void init() {
-        // new implementation for test purposes
-//        int rows = 3;
-//        int cols = 3;
-//        cells = new Cell[rows][cols];
-//        int row = 0;
-//        int col = 0;
-//        for (Cell[] line : cells) {
-//            for (Cell cell : line) {
-//                cell = new Cell(row, col);
-//            }
-//        }
-        Cell[] firstRow = {new Cell(0, 0), new Cell(0, 1), new Cell(0, 2)};
-        Cell[] secondRow = {new Cell(1, 0), new Cell(1, 1), new Cell(1, 2)};
-        Cell[] thirdRow = {new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)};
-        cells = new Cell[][]{firstRow, secondRow, thirdRow};
+        cells = new Cell[3][3];
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                Cell cell = new Cell(i, j);
+                cells[i][j] = cell;
+            }
+        }
     }
 
     public Cell[][] getCells() {
