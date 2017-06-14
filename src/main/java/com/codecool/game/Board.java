0e133a03fc44dca6522a0f9741a2a647f9bc3841
Marta Sajdak <crappylime@gmail.com>
2017-06-14 11:53:53 +0200

    Try new implementation of init

--- src/main/java/com/codecool/game/Board.java
+++ src/main/java/com/codecool/game/Board.java
@@ -4,6 +4,17 @@ public class Board {
     private Cell[][] cells;
 
     public void init() {
+        // new implementation for test purposes
+//        int rows = 3;
+//        int cols = 3;
+//        cells = new Cell[rows][cols];
+//        int row = 0;
+//        int col = 0;
+//        for (Cell[] line : cells) {
+//            for (Cell cell : line) {
+//                cell = new Cell(row, col);
+//            }
+//        }
         Cell[] firstRow = {new Cell(0, 0), new Cell(0, 1), new Cell(0, 2)};
         Cell[] secondRow = {new Cell(1, 0), new Cell(1, 1), new Cell(1, 2)};
         Cell[] thirdRow = {new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)};
