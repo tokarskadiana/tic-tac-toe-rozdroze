d1f9906c341813a53182359cc40a2f85d4232d77
Marta Sajdak <crappylime@gmail.com>
2017-06-14 11:39:05 +0200

    Implement init() and getCells() in Board

--- src/main/java/com/codecool/game/Board.java
+++ src/main/java/com/codecool/game/Board.java
@@ -1,4 +1,16 @@
 package com.codecool.game;
 
 public class Board {
+    private Cell[][] cells;
+
+    public void init() {
+        Cell[] firstRow = {new Cell(0, 0), new Cell(0, 1), new Cell(0, 2)};
+        Cell[] secondRow = {new Cell(1, 0), new Cell(1, 1), new Cell(1, 2)};
+        Cell[] thirdRow = {new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)};
+        cells = new Cell[][]{firstRow, secondRow, thirdRow};
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
 }
