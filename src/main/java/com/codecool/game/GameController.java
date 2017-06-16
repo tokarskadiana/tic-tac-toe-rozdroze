e2152b2c7e2474a63aa757d0c95bc041ebb263f8
Diana Tokarska <tokarskadiana@gmail.com>
2017-06-16 05:36:19 +0200

    Create GameController

---
+++ src/main/java/com/codecool/game/GameController.java
@@ -0,0 +1,26 @@
+package com.codecool.game;
+
+public class GameController {
+
+    Game game;
+    UserInterface ui;
+
+    public GameController(Game game, UserInterface ui) {
+        this.game = game;
+        this.ui = ui;
+    }
+
+    public void  runGame() {
+        game.initGame();
+        ui.displayFirstPlayer();
+        do {
+            ui.printBoard();
+            int[] userMove = ui.getUserMove();
+            System.out.println(userMove[0]);
+            System.out.println(userMove[1]);
+            game.updateGameState(game.getCurrentPlayer(),userMove[0], userMove[1]);
+        } while (game.getCurrentState() == GameState.PLAYING);
+        ui.printBoard();
+        ui.displayWinner(game.getCurrentPlayer());
+    }
+}
